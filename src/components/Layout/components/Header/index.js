cde007eeed515791d64638c86bf205f9652d84a9
minhvu278 <dominhvu278@gmail.com>
2022-10-30 22:57:32 +0700

    code header language

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -18,16 +18,31 @@ const cx = classNames.bind(styles)
 
 const MENU_ITEMS = [
     {
-        icon: <FontAwesomeIcon icon={faEarthAsia} />,
-        title: 'English'
+        icon: <FontAwesomeIcon icon={faEarthAsia}/>,
+        title: 'English',
+        children: {
+            title: 'Language',
+            data: [
+                {
+                    type: 'language',
+                    code: 'en',
+                    title: 'English'
+                },
+                {
+                    type: 'language',
+                    code: 'vi',
+                    title: 'Vietnamese'
+                }
+            ]
+        }
     },
     {
-        icon: <FontAwesomeIcon icon={faQuestion} />,
+        icon: <FontAwesomeIcon icon={faQuestion}/>,
         title: 'Feedback and help',
         to: '/feedback'
     },
     {
-        icon: <FontAwesomeIcon icon={faKeyboard} />,
+        icon: <FontAwesomeIcon icon={faKeyboard}/>,
         title: 'Keyboard shortcuts'
     }
 ]
@@ -41,6 +56,14 @@ function Header() {
             setSearchResult([])
         }, 0)
     }, [])
+
+    const handleMenuChange = (menuItem) => {
+        switch (menuItem.type) {
+            case 'language':
+                break;
+            default:
+        }
+    }
 
     return (
         <header className={cx('wrapper')}>
@@ -77,7 +100,7 @@ function Header() {
                     <Button text>Upload</Button>
                     <Button primary>Login</Button>
 
-                    <Menu items={MENU_ITEMS}>
+                    <Menu items={MENU_ITEMS} onChange={handleMenuChange}>
                         <button className={cx('more-btn')}>
                             <FontAwesomeIcon icon={faEllipsisVertical}/>
                         </button>
