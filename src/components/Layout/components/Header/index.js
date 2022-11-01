476ec8c0a50eba3c1157c004137a091c10fc5bc2
hapo-vudm <dominhvu278@gmail.com>
2022-11-01 09:27:08 +0700

    config route and fix search & click profile

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -15,6 +15,8 @@ import {faKeyboard} from "@fortawesome/free-solid-svg-icons/faKeyboard";
 import Image from "~/components/Image";
 import {InboxIcon, MessageIcon, UploadIcon} from "~/components/Icons";
 import Search from "~/components/Layout/components/Search";
+import {Link} from "react-router-dom";
+import routesConfig from "~/config/routes";
 
 const cx = classNames.bind(styles)
 
@@ -89,7 +91,7 @@ function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                <img src={images.logo} alt="Tiktok"/>
+                <Link to={routesConfig.home} className={cx('logo-link')}><img src={images.logo} alt="Tiktok"/></Link>
                 <Search />
 
                 <div className={cx('actions')}>
--- src/components/routes/index.js
+++ src/components/routes/index.js
@@ -4,13 +4,14 @@ import Profile from "~/components/pages/Profile";
 import Upload from "~/components/pages/Upload";
 import {HeaderOnly} from "~/components/Layout";
 import Search from "~/components/pages/Search";
+import routesConfig from "~/config/routes";
 
 const publicRoutes = [
-    {path: '/', component: Home},
-    {path: '/following', component: Following},
-    {path: '/@:nickname', component: Profile},
-    {path: '/upload', component: Upload, layout: HeaderOnly},
-    {path: '/search', component: Search, layout: null},
+    {path: routesConfig.home, component: Home},
+    {path: routesConfig.following, component: Following},
+    {path: routesConfig.profile, component: Profile},
+    {path: routesConfig.upload, component: Upload, layout: HeaderOnly},
+    {path: routesConfig.search, component: Search, layout: null},
 ]
 
 const privateRoutes = [
